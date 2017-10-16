b3a9087d0eb5155acdd98869cc48d85ea5a481a3
xHeliotrope <rmoore8869@gmail.com>
2017-10-16 18:13:15 +0000

    integrated socketio

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-var motor = require("./motor")
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+var motor = require('./motor')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -8,13 +10,20 @@ app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
+io.on('connection', function(socket){
+  console.log('a user connected')
+})
 
 app.get('/', function(req, res) {
     res.render('pages/index');
-});
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port ', app.get('port'));
-});
+io.on('connection', function(socket){
+  console.log('a user connected')
+})
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port ', app.get('port'))
+})
 
 
