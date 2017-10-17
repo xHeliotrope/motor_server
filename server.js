e699f7c13e16f4a8d3fa48751686136369da57f0
xHeliotrope <rmoore8869@gmail.com>
2017-10-17 15:44:44 +0000

    feature/fix: getting the twitter side of this operation running

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-var motor = require('./motor')
+require('dotenv').config()
+//var motor = require('./motor')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -10,16 +11,16 @@ app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-io.on('connection', function(socket){
-  console.log('a user connected')
-})
-
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
     res.render('pages/index');
 })
 
 io.on('connection', function(socket){
+  console.log(process.env.access_token);
   console.log('a user connected')
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  })
 })
 
 http.listen(app.get('port'), function() {
